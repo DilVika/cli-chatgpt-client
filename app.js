495e7170b49fe281d02a511167d3d1e2da1fa1ab
Dilvika <dilvika@users.noreply.github.com>
2023-03-26 23:11:40 +0700

    add. adding typing fx

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const readline = require('readline');
 require('dotenv').config();
 const { PassThrough } = require('stream');
 
+const { CONSOLE_COLORS } = require('./helpers');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -12,14 +14,18 @@ const API_KEY = process.env.OPENAI_API_KEY;
 const GPT_URL = 'https://api.openai.com/v1/chat/completions';
 
 
-const stream = new PassThrough();
 
 const prompt = () => {
+    const stream = new PassThrough();
+    console.log(CONSOLE_COLORS.fgWhite, '');
     rl.question('You: ', (message) => {
         if (message === 'exit') {
+            stream.end(true);
             rl.close();
             return;
         }
+        CONSOLE_COLORS.setColor(CONSOLE_COLORS.fgCyan);
+        process.stdout.write('ChatGPT: ');
         axios.post(GPT_URL, {
             model: "gpt-3.5-turbo",
             temperature: 0.7,
@@ -37,42 +43,42 @@ const prompt = () => {
         })
             .then(response => {
                 response.data.pipe(stream);
-                console.log('added');
-                printTheAnswer();
-                // console.log('\x1b[36m%s\x1b[0m', `ChatGPT: ${response.data.choices[0].message.content}`);
-                // prompt();
+                // console.log('added');
+                printTheAnswer(stream);
             })
             .catch(error => {
-                // console.log(`Bearer ${temp_key}`)
-                // console.error(error);
-                console.log(error.data);
+                console.error(error.response.status);
+                console.error(error.response.statusText);
                 prompt();
             });
     });
 };
 
-const printTheAnswer = () => {
-    stream.on('end', () => {
-        console.log('Stream ended');
-        prompt();
+const printTheAnswer = (stream) => {
+    stream.on('end', (isTerminated) => {
+        console.warn(CONSOLE_COLORS.fgYellow, '<Stream ended>');
+        if (!isTerminated) {
+            prompt();
+        }
     });
-
+    // process.stdout.write('ChatGPT: ');
     stream.on('data', (data) => {
 
         if (data.toString().includes("[DONE]")) {
-            // prompt();
-            console.log('done data');
+            console.info(CONSOLE_COLORS.fgGreen, '\n<Answer is ready>');
         } else {
             const prepareStr = data.toString().replace("data: ", '');
             const responseObj = JSON.parse(prepareStr);
-            console.log('\x1b[36m%s\x1b[0m', `ChatGPT: ${responseObj.choices[0].delta.content}`);
+            // console.log('\x1b[36m%s\x1b[0m', `ChatGPT: ${responseObj.choices[0].delta.content}`);
+            // Cheeck if the answer is ready, valid data, not undefined or null or an object
+            if (responseObj.choices[0].delta.content) {
+                process.stdout.write(responseObj.choices[0].delta.content);
+            }
         }
     });
 
 
-}
-
-
+};
 
 
 prompt();
---
+++ helpers.js
@@ -0,0 +1,27 @@
+const CONSOLE_COLORS = {
+    fgRed: "\x1b[31m",
+    fgGreen: "\x1b[32m",
+    fgYellow: "\x1b[33m",
+    fgBlue: "\x1b[34m",
+    fgMagenta: "\x1b[35m",
+    fgCyan: "\x1b[36m",
+    fgWhite: "\x1b[37m",
+    fgGray: "\x1b[90m",
+    bgBlack: "\x1b[40m",
+    bgRed: "\x1b[41m",
+    bgGreen: "\x1b[42m",
+    bgYellow: "\x1b[43m",
+    bgBlue: "\x1b[44m",
+    bgMagenta: "\x1b[45m",
+    bgCyan: "\x1b[46m",
+    bgWhite: "\x1b[47m",
+    bgGray: "\x1b[100m",
+    reset: "\x1b[0m",
+
+    // Set Color for process.stdout.write
+    setColor: (color) => {
+        console.log(color, '');
+    }
+};
+
+module.exports = {CONSOLE_COLORS};
